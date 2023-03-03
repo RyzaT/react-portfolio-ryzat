d48f7554e4f6ad5ce5d1af7fe4c21e4c72dcaf08
RyzaT <ryantaylor50@hotmail.com>
2023-03-03 13:01:56 +0000

    Home.js basic code

---
+++ components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from  'react';
+
+function Home () {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <p>
+                Here will go  some info about me.
+                Yada yada yada 
+                Yada de do.
+            </p>
+            {/* Picture or Avatar somewhere around here */}
+        </div>
+    );
+}
+ export default Home;
