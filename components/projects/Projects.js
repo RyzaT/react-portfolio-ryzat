d60277bbc1e0bb3d134983f5c42cd4e9e1e9c714
RyzaT <ryantaylor50@hotmail.com>
2023-03-03 14:45:36 +0000

    added map and project json boiler

--- components/projects/Projects.js
+++ components/projects/Projects.js
@@ -1,10 +1,37 @@
 import React from "react";
 import "./projects.css";
+import "./projects.json";
+
+class Projects extends Component {
+    // Setting this.state.friends to the friends json array
+    // 
+    state = {
+      projects
+    };
+
+render() {
+    return (
+      <Wrapper>
+        <Title>Project List</Title>
+        {this.state.projects.map(project => (
+          <Projects
+            // removeFriend={this.removeFriend}
+            id={project.id}
+            key={project.id}
+            name={project.name}
+            image={project.image}
+            link={project.link}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
 
 /* Lots of editing needed here */
 function Projects(props) {
   return (
-    <div className="card">
+    <div className="ProjectCard">
       <div className="img-container">
         {/* <img alt={props.name} src={props.image} /> */}
       </div>
@@ -27,5 +54,6 @@ function Projects(props) {
     </div>
   );
 }
+
 
 export default Projects;
